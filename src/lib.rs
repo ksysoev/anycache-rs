a00ef9284b62c310e7a6980b3626ca7bf1991d09
Кирилл Сысоев <k.sysoev@me.com>
2023-09-25 22:29:04 +0800

    Adds cache stuct

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+pub trait Storable {
+    fn get(&self, key: &str) -> Option<String>;
+    fn set(&self, key: &str, value: &str);
+    fn ttl(&self, key: &str) -> Option<u64>;
+    fn del(&self, key: &str);
+}
+
+pub struct Cache {
+    storage: Box<dyn Storable>, // Can we do this without dynamic dispatch?
+}
+
+impl Cache {
+    pub fn new(storage: Box<dyn Storable>) -> Self {
+        Self { storage }
+    }
+
+    pub fn cache(&self, key: String, get_data: Box<dyn FnOnce() -> String>) -> String {
+        let data = self.storage.get(&key);
+        match data {
+            Some(data) => data,
+            None => {
+                let data = get_data();
+                self.storage.set(&key, &data);
+                data
+            }
+        }
+    }
+}
