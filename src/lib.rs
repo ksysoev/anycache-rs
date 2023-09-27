7a22ded1e2a9236eb87531ea624edc862f75dc41
Кирилл Сысоев <k.sysoev@me.com>
2023-09-27 20:27:46 +0800

    Implements async logic at Cache stuct

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,32 @@
 mod memory;
 
+use std::future::Future;
+
 pub trait Storable {
     fn get(&self, key: &str) -> Option<String>;
     fn set(&self, key: &str, value: &str);
     fn del(&self, key: &str);
 }
 
-pub struct Cache {
-    storage: Box<dyn Storable>, // Can we do this without dynamic dispatch?
+pub struct Cache<S: Storable> {
+    storage: S,
 }
 
-impl Cache {
-    pub fn new(storage: Box<dyn Storable>) -> Self {
+impl<S: Storable> Cache<S> {
+    pub fn new<T>(storage: S) -> Self {
         Self { storage }
     }
 
-    pub fn cache(&self, key: String, get_data: Box<dyn FnOnce() -> String>) -> String {
+    pub async fn cache<F, Fut>(&self, key: String, get_data: F) -> String
+    where
+        F: Fn() -> Fut,
+        Fut: Future<Output = String>,
+    {
         let data = self.storage.get(&key);
         match data {
             Some(data) => data,
             None => {
-                let data = get_data();
+                let data = get_data().await;
                 self.storage.set(&key, &data);
                 data
             }
@@ -32,37 +38,44 @@ impl Cache {
     }
 }
 
-#[cfg(test)]
 mod tests {
-    #[test]
-    fn cache() {
+    #[tokio::test]
+    async fn cache() {
         use super::memory::InMemoryStorage;
         use super::Cache;
 
         let storage = InMemoryStorage::new(10);
-        let cache = Cache::new(Box::new(storage));
+        let cache = Cache::new::<InMemoryStorage>(storage);
 
-        let data = cache.cache("foo".to_string(), Box::new(|| "test".to_string()));
+        let data = cache
+            .cache("foo".to_string(), || async { "test".to_string() })
+            .await;
         assert_eq!(data, "test".to_string());
 
-        let data = cache.cache("foo".to_string(), Box::new(|| "test2".to_string()));
+        let data = cache
+            .cache("foo".to_string(), || async { "test2".to_string() })
+            .await;
         assert_eq!(data, "test".to_string());
     }
 
-    #[test]
-    fn invalidate() {
+    #[tokio::test]
+    async fn invalidate() {
         use super::memory::InMemoryStorage;
         use super::Cache;
 
         let storage = InMemoryStorage::new(10);
-        let cache = Cache::new(Box::new(storage));
+        let cache = Cache::new::<InMemoryStorage>(storage);
 
-        let data = cache.cache("foo".to_string(), Box::new(|| "test".to_string()));
+        let data = cache
+            .cache("foo".to_string(), || async { "test".to_string() })
+            .await;
         assert_eq!(data, "test".to_string());
 
         cache.invalidate("foo".to_string());
 
-        let data = cache.cache("foo".to_string(), Box::new(|| "test2".to_string()));
+        let data = cache
+            .cache("foo".to_string(), || async { "test2".to_string() })
+            .await;
         assert_eq!(data, "test2".to_string());
     }
 }
